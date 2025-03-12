d5546cde19c07595fdf70e85f8f2926afa1f1685
yarbisbeme <yarbisbeme@gmail.com>
2025-03-12 13:00:04 -0400

    first testing

--- first-practice/main.py
+++ first-practice/main.py
@@ -1,7 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import time
 
 service = Service(executable_path="chromedriver.exe");
 driver = webdriver.Chrome(service=service);
-driver.maximize_window()
+driver.get("http://selenium.dev/")
 
+title = driver.title
+print(title);
+
+"""
+    Verdadero
+"""
+assert "Selenium" in title;
+driver.quit();
